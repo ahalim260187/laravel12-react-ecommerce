a2d8d20c27dfa08804a635aac49c2775b4943261
Abdul Halim <raffahaitami@gmail.com>
2025-09-20 21:54:06 +0700

    Install and Activated DaisyUI

--- resources/js/Pages/Profile/Edit.tsx
+++ resources/js/Pages/Profile/Edit.tsx
@@ -5,10 +5,11 @@ import DeleteUserForm from './Partials/DeleteUserForm';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
 
-export default function Edit({
-                               mustVerifyEmail,
-                               status,
-                             }: PageProps<{ mustVerifyEmail: boolean; status?: string }>) {
+export default function Edit(
+  {
+    mustVerifyEmail,
+    status,
+  }: PageProps<{ mustVerifyEmail: boolean; status?: string }>) {
   return (
     <AuthenticatedLayout
       header={
